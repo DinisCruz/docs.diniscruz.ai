207b4fcfac9c7418b2862b23429dd4e4f3738fe4
Dinis Cruz <dinis.cruz@owasp.org>
2025-03-31 17:14:19 +0100

    fixed download path (to include GitHub)

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 def define_env(env):
     @env.macro
     def download_pdf(file_date, file_name, label="Download"):
-        url = f'https://files.diniscruz.ai/pdfs/{file_date}/{file_name}'
+        url = f'https://files.diniscruz.ai/github/pdfs/{file_date}/{file_name}'
         return f'[:fontawesome-solid-file-pdf: {label}]({url}){{ .md-button .md-button--primary .md-button--small .no-print}}'
