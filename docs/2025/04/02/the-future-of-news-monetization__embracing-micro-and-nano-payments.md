c776921b44d386e623816b0f5ebe114be30dd0f5
Dinis Cruz <dinis.cruz@owasp.org>
2025-04-02 23:22:24 +0100

    added show_spotify_ui and show_infographic

--- docs/2025/04/02/the-future-of-news-monetization__embracing-micro-and-nano-payments.md
+++ docs/2025/04/02/the-future-of-news-monetization__embracing-micro-and-nano-payments.md
@@ -11,12 +11,9 @@ _by {{ authors | join(" and ") }}, {{ date }}_
 
 {{ download_pdf(date, pdf_file) }} {{ download_wav(date, wav_file) }}
 
-## Podcast
+{{ show_spotify_ui(spotify_id) }}
 
-<iframe src="https://open.spotify.com/embed/episode/{{spotify_id}}?utm_source=generator" 
-        width="100%" height="152" frameborder="0" 
-        allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-        loading="lazy"></iframe>
+{{ show_infographic(date, pdf_file) }}
 
 
 ## Executive Summary
--- main.py
+++ main.py
@@ -9,3 +9,23 @@ def define_env(env):
     def download_wav(file_date, file_name, label="Listen Audio"):
         url = f'https://files.diniscruz.ai/s3/wav/{file_date}/{file_name}'
         return f'[:fontawesome-solid-headphones: {label}]({url}){{ .md-button .md-button--primary .md-button--small .no-print}}'
+
+
+    @env.macro
+    def show_spotify_ui(spotify_id):
+        return f'''
+## Podcast
+<iframe src="https://open.spotify.com/embed/episode/{spotify_id}?utm_source=generator"
+        width="100%" height="152" frameborder="0"
+        allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"></iframe>
+'''
+
+    @env.macro
+    def show_infographic(file_date, file_name):
+        url = f'https://files.diniscruz.ai/s3/pdf/{file_date}/infographic__{file_name}'
+        return f'''
+## Infographic    
+<iframe src="{url}"
+        width="100%" height="512px" style="border: none;"></iframe>
+'''
